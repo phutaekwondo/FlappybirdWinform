7d49d51ab68a4d8dff89ed67ed43a0cd46f80ea4
Vinh Phu <19522020@gm.uit.edu.vn>
2021-11-16 16:18:30 +0700

    move up and down (just for test key event), declare an event for gameTime.Tick but not thing to do init yet

--- FlappyBird/mainForm.Designer.cs
+++ FlappyBird/mainForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace FlappyBird
 {
-    partial class Form1
+    partial class mainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,9 +29,12 @@ namespace FlappyBird
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.picGround = new System.Windows.Forms.PictureBox();
             this.picDesktop = new System.Windows.Forms.PictureBox();
             this.picBird = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.labDebug = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picGround)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picDesktop)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBird)).BeginInit();
@@ -43,7 +46,7 @@ namespace FlappyBird
             | System.Windows.Forms.AnchorStyles.Right)));
             this.picGround.Image = global::FlappyBird.Properties.Resources.ground;
             this.picGround.Location = new System.Drawing.Point(0, 593);
-            this.picGround.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picGround.Margin = new System.Windows.Forms.Padding(2);
             this.picGround.Name = "picGround";
             this.picGround.Size = new System.Drawing.Size(501, 117);
             this.picGround.TabIndex = 1;
@@ -56,7 +59,7 @@ namespace FlappyBird
             | System.Windows.Forms.AnchorStyles.Right)));
             this.picDesktop.Image = global::FlappyBird.Properties.Resources.sky;
             this.picDesktop.Location = new System.Drawing.Point(0, 0);
-            this.picDesktop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picDesktop.Margin = new System.Windows.Forms.Padding(2);
             this.picDesktop.Name = "picDesktop";
             this.picDesktop.Size = new System.Drawing.Size(501, 620);
             this.picDesktop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -69,28 +72,48 @@ namespace FlappyBird
             this.picBird.BackColor = System.Drawing.Color.Transparent;
             this.picBird.Image = global::FlappyBird.Properties.Resources.birdscaled;
             this.picBird.Location = new System.Drawing.Point(55, 300);
-            this.picBird.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picBird.Margin = new System.Windows.Forms.Padding(2);
             this.picBird.Name = "picBird";
             this.picBird.Size = new System.Drawing.Size(66, 48);
             this.picBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picBird.TabIndex = 2;
             this.picBird.TabStop = false;
             // 
-            // Form1
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // labDebug
+            // 
+            this.labDebug.AutoSize = true;
+            this.labDebug.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labDebug.Location = new System.Drawing.Point(22, 22);
+            this.labDebug.Name = "labDebug";
+            this.labDebug.Size = new System.Drawing.Size(48, 18);
+            this.labDebug.TabIndex = 3;
+            this.labDebug.Text = "debug";
+            // 
+            // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(501, 710);
+            this.Controls.Add(this.labDebug);
             this.Controls.Add(this.picBird);
             this.Controls.Add(this.picGround);
             this.Controls.Add(this.picDesktop);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.Name = "Form1";
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "mainForm";
             this.Text = "flappy bird";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.mainForm_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.mainForm_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.picGround)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picDesktop)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBird)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -99,6 +122,8 @@ namespace FlappyBird
         private System.Windows.Forms.PictureBox picDesktop;
         private System.Windows.Forms.PictureBox picGround;
         private System.Windows.Forms.PictureBox picBird;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label labDebug;
     }
 }
 
---
+++ FlappyBird/mainForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBird
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        // PHU ZONE
+        void init()
+        {
+            // made a picture box for bird
+            picBird.Parent = picDesktop;
+            picBird.BackColor = Color.Transparent;
+            
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            //move the bird 
+            if ( e.KeyCode == Keys.Up)
+            {
+                //move up
+                int bird_x = picBird.Location.X;
+                int bird_y = picBird.Location.Y;
+
+                bird_y -= 10;
+
+                picBird.Location = new Point(bird_x, bird_y);
+            }
+            else if ( e.KeyCode == Keys.Down)
+            {
+                //move down 
+                int bird_x = picBird.Location.X;
+                int bird_y = picBird.Location.Y;
+
+                bird_y += 10;
+
+                picBird.Location = new Point(bird_x, bird_y);
+            }
+        }
+
+        private void mainForm_KeyUp(object sender, KeyEventArgs e)
+        {
+        }
+
+        // END PHU ZONE
+
+        // TU ZONE  
+        // END TU ZONE
+    }
+}
